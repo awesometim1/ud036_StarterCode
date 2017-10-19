fa1624d0ff60c17dc104e57f6566355a7782e34e
awesometim1 <awesometim1@gmail.com>
2017-10-19 08:18:32 -0400

    Make entertainment_center.py PEP8 format

--- entertainment_center.py
+++ entertainment_center.py
@@ -9,37 +9,33 @@ import fresh_tomatoes
 # Instantiates 6 movie objects
 
 toy_story = media.Movie('Toy Story',
-                        'A story of a boy and his toys that come to life'
-                        ,
-                        'http://www.gstatic.com/tv/thumb/movieposters/17420/p17420_p_v8_ab.jpg'
-                        , 'https://www.youtube.com/watch?v=KYz2wyBy3kc')
+                        'A story of a boy and his toys that come to life',
+                        'http://bit.ly/1qM8Na0',
+                        'https://www.youtube.com/watch?v=KYz2wyBy3kc')
 
 avatar = media.Movie('Avatar', 'A marine on an alien planet',
-                     'http://t0.gstatic.com/images?q=tbn:ANd9GcQCfmvrE4fMo2cd8esc7mDZPtFSJThAujddMPkRtti1_ij6u-jp'
-                     , 'https://www.youtube.com/watch?v=5PSNL1qE6VY')
+                     'http://bit.ly/1O2b493',
+                     'https://www.youtube.com/watch?v=5PSNL1qE6VY')
 
 ai = media.Movie('AI',
-                 'A robotic boy embarks on a journey to find his true self'
-                 ,
-                 'http://www.gstatic.com/tv/thumb/movieposters/27945/p27945_p_v8_ai.jpg'
-                 , 'https://www.youtube.com/watch?v=oBUAQGwzGk0')
+                 'A robotic boy embarks on a journey to find his true self',
+                 'http://bit.ly/2yC0GET',
+                 'https://www.youtube.com/watch?v=oBUAQGwzGk0')
 
 rat = media.Movie('Ratatouille',
                   'A rat who loves tasting food and befriends a cook',
-                  'http://www.gstatic.com/tv/thumb/dvdboxart/165058/p165058_d_v8_aa.jpg'
-                  , 'https://www.youtube.com/watch?v=c3sBBRxDAqk')
+                  'http://bit.ly/2xTg1l0',
+                  'https://www.youtube.com/watch?v=c3sBBRxDAqk')
 
 indiana = media.Movie('Indiana Jones: The Last Crusade',
-                      'Indiana Jones goes on a quest to find the magical goblet'
-                      ,
-                      'http://t1.gstatic.com/images?q=tbn:ANd9GcTvapMUYGkuF5IrOsugBN3-kAIMHEhGu2exzQErGjVMiXCHtCmA'
-                      , 'https://www.youtube.com/watch?v=a6JB2suJYHM')
+                      'Indiana Jones goes on a quest',
+                      'http://bit.ly/2ytvPdR',
+                      'https://www.youtube.com/watch?v=a6JB2suJYHM')
 
 wonder = media.Movie('Wonder Woman',
-                     'She fights a war to end all wars and discovers her true destiny'
-                     ,
-                     'http://t1.gstatic.com/images?q=tbn:ANd9GcQcCAOmt-FsRsR8GebIzI67qSvdQ2JLYDRLxeAcbH-541fzqq1H'
-                     , 'https://www.youtube.com/watch?v=VSB4wGIdDwo')
+                     'She fights a war to end all wars',
+                     'http://bit.ly/2rRZASN',
+                     'https://www.youtube.com/watch?v=VSB4wGIdDwo')
 
 # Makes a list of movie objects
 
