a778c4a4658351068617e87fdb070ba65c217d88
awesometim1 <awesometim1@gmail.com>
2017-10-19 08:08:39 -0400

    Beautify media.py

--- media.py
+++ media.py
@@ -1,15 +1,23 @@
 import webbrowser
 
-class Movie():
 
+class Movie:
 
-    #Initializes the Movie Object
-    def __init__(self , movie_title, movie_storyline, poster_image, trailer_youtube):
+    # Initializes the Movie Object
+
+    def __init__(
+        self,
+        movie_title,
+        movie_storyline,
+        poster_image,
+        trailer_youtube,
+                ):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
-    #Opens the trailer of the Movie object
+    # Opens the trailer of the Movie object
+
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
